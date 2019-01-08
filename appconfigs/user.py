9f9cb552068087faed16fe02989c2afb0a94fc84
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-01-08 16:32:22 -0500

    Add user.py

---
+++ appconfigs/user.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © Spyder Project Contributors
+#
+# This file is a derivative work of codes taken from the
+# Spyder project, licensed under the terms of the MIT License.
+# (https://github.com/spyder-ide/spyder)
+# -----------------------------------------------------------------------------
+
+"""
+This module provides user configuration file management features for Python
+applications. It is based on the ConfigParser class of the configparser
+module, which is available in the standard library.
+"""
+
