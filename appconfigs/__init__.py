ca4d4b557546dc6367b2fa935a06671ee116f82a
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-01-08 16:31:42 -0500

    Add __init__.py

---
+++ appconfigs/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © Spyder Project Contributors
+#
+# This file is a derivative work of codes from the Spyder project,
+# licensed under the terms of the MIT License
+# (see spyder/__init__.py for details)
+# -----------------------------------------------------------------------------
+
+version_info = (0, 1, 0, 'dev0')
+__version__ = '.'.join(map(str, version_info))
+__date__ = '08/01/2018'
+__project_url__ = "https://github.com/jnsebgosselin/appconfigs"
