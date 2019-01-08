720a77a3ddeba2fe513e7d3c8c2877f902157412
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-01-08 16:34:19 -0500

    Add class DefaultsConfig
    Class used to save default config options to a file.

--- appconfigs/user.py
+++ appconfigs/user.py
@@ -29,3 +29,60 @@ class NoDefault:
     def __repr__(self):
         return '<default value not set>'
 
+
+class DefaultsConfig(cp.ConfigParser):
+    """Class used to save default config options to a file."""
+
+    def __init__(self, name, path):
+        cp.ConfigParser.__init__(self, interpolation=None)
+        self.name = name
+        self.path = path
+
+    def _set(self, section, option, value, verbose):
+        """
+        Private set method
+        """
+        if not self.has_section(section):
+            self.add_section(section)
+        if verbose:
+            print('%s[ %s ] = %s' % (section, option, value))
+        cp.ConfigParser.set(self, section, option, value)
+
+    def _write(self, filename):
+        """
+        Write file to disk.
+        """
+        with open(filename, 'w', encoding='utf-8') as inifile:
+            self.write(inifile)
+
+    def _save(self):
+        """
+        Save config into the associated .ini file
+        """
+        filename = self.get_filename()
+        if not osp.exists(osp.dirname(filename)):
+            os.makedirs(osp.dirname(filename))
+
+        try:
+            self._write(filename)
+        except EnvironmentError:
+            try:
+                if osp.isfile(filename):
+                    os.remove(filename)
+                time.sleep(0.05)
+                self._write(filename)
+            except Exception as e:
+                print("Failed to write user configuration file to disk, with "
+                      "the exception shown below")
+                print(e)
+
+    def get_filename(self):
+        """Return the name of the configuration file to use."""
+        return osp.join(self.path, '{}.ini'.format(self.name))
+
+    def set_defaults(self, defaults):
+        """Set default config values."""
+        for section, options in defaults:
+            for option, new_value in options.items():
+                self._set(section, option, new_value, False)
+
