20bcee383df07a4b5401c6e12c97d3e3c4933708
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-01-09 00:02:24 -0500

    Add imports, constant, and tempdir fixture

--- appconfigs/tests/test_userconfig.py
+++ appconfigs/tests/test_userconfig.py
@@ -5,3 +5,30 @@
 # (https://github.com/jnsebgosselin/appconfigs)
 # -----------------------------------------------------------------------------
 
+# ---- Standard imports
+import os.path as osp
+import filecmp
+
+# ---- Third party imports
+import pytest
+
+# ---- Local imports
+from appconfigs.user import UserConfig
+
+NAME = 'user_config_tests'
+CONF_VERSION = '0.1.0'
+DEFAULTS = [
+    ('main',
+        {'first_option': 'value',
+         'second_option': 24.567,
+         'third_option': 22,
+         'fourth_option': True}
+     )
+]
+
+
+# ---- Pytest fixtures
+@pytest.fixture
+def configdir(tmpdir):
+    return osp.join(str(tmpdir), 'UserConfigTests')
+
