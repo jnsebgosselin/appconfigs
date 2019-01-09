8753aabb610536cce8c186036522a567b4fec2bc
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-01-09 00:01:56 -0500

    Add test_userconfig.py

---
+++ appconfigs/tests/test_userconfig.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © Jean-Sébastien Gosselin
+# Licensed under the terms of the MIT License
+# (https://github.com/jnsebgosselin/appconfigs)
+# -----------------------------------------------------------------------------
+
