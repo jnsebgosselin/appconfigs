c38c31f0ae5cf9970b09ee3c6052153458ecd8ea
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-06-14 15:28:36 -0400

    Bump version to 0.1.1 for release

--- appconfigs/__init__.py
+++ appconfigs/__init__.py
@@ -7,7 +7,7 @@
 # (see spyder/__init__.py for details)
 # -----------------------------------------------------------------------------
 
-version_info = (0, 2, 0, 'dev0')
+version_info = (0, 1, 1)
 __version__ = '.'.join(map(str, version_info))
 __date__ = '14/06/2019'
 __project_url__ = "https://github.com/jnsebgosselin/appconfigs"
